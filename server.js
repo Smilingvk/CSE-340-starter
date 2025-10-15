f51c06343b83ad183ed3c80ec3b5fccd1b8589bd
Smilingvk <valeriaguzmanavalos@gmail.com>
2025-10-14 22:02:49 -0500

    error handling

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
@@ -10,6 +10,9 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
 
 /* ***********************
  * View Engine and Templates
@@ -18,7 +21,45 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
+/* ***********************
+ * Routes
+ *************************/
 app.use(static)
+
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+// Intentional Error Route (Task 3)
+app.get("/trigger-error", utilities.handleErrors((req, res, next) => {
+  throw new Error("Intentional 500 error triggered for testing purposes")
+}))
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ 
+    message = err.message
+  } else {
+    message = 'Oh no! There was a crash. Maybe try a different route?'
+  }
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
@@ -32,10 +73,4 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
-})
-/**
- * Index route
- */
-app.get("/", function(req,res){
-  res.render("index", {title:"Home"})
 })
---
+++ utilities/index.js
@@ -0,0 +1,106 @@
+const invModel = require("../models/inventory-model")
+const Util = {}
+
+/* ************************
+ * Constructs the nav HTML unordered list
+ ************************** */
+Util.getNav = async function (req, res, next) {
+  let data = await invModel.getClassifications()
+  let list = "<ul>"
+  list += '<li><a href="/" title="Home page">Home</a></li>'
+  data.rows.forEach((row) => {
+    list += "<li>"
+    list +=
+      '<a href="/inv/type/' +
+      row.classification_id +
+      '" title="See our inventory of ' +
+      row.classification_name +
+      ' vehicles">' +
+      row.classification_name +
+      "</a>"
+    list += "</li>"
+  })
+  list += "</ul>"
+  return list
+}
+
+/* **************************************
+* Build the classification view HTML
+* ************************************ */
+Util.buildClassificationGrid = async function(data){
+  let grid
+  if(data.length > 0){
+    grid = '<ul id="inv-display">'
+    data.forEach(vehicle => { 
+      grid += '<li>'
+      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+      + 'details"><img src="' + vehicle.inv_thumbnail 
+      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      +' on CSE Motors" /></a>'
+      grid += '<div class="namePrice">'
+      grid += '<hr />'
+      grid += '<h2>'
+      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+      grid += '</h2>'
+      grid += '<span>$' 
+      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+      grid += '</div>'
+      grid += '</li>'
+    })
+    grid += '</ul>'
+  } else { 
+    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+  }
+  return grid
+}
+
+/* **************************************
+* Build the detail view HTML
+* ************************************ */
+Util.buildDetailView = async function(vehicle){
+  let detailView = '<div class="vehicle-detail">'
+  
+  // Image section
+  detailView += '<div class="vehicle-image-section">'
+  detailView += '<img src="' + vehicle.inv_image 
+  + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model 
+  + '" class="vehicle-detail-image" />'
+  detailView += '</div>'
+  
+  // Info section
+  detailView += '<div class="vehicle-info-section">'
+  detailView += '<h2>' + vehicle.inv_year + ' ' + vehicle.inv_make + ' ' + vehicle.inv_model + '</h2>'
+  
+  // Price
+  detailView += '<p class="vehicle-price"><strong>Price: </strong>$' 
+  + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</p>'
+  
+  // Description
+  detailView += '<p class="vehicle-description"><strong>Description: </strong>' 
+  + vehicle.inv_description + '</p>'
+  
+  // Color
+  detailView += '<p class="vehicle-color"><strong>Color: </strong>' 
+  + vehicle.inv_color + '</p>'
+  
+  // Mileage
+  detailView += '<p class="vehicle-mileage"><strong>Mileage: </strong>' 
+  + new Intl.NumberFormat('en-US').format(vehicle.inv_miles) + ' miles</p>'
+  
+  detailView += '</div>'
+  detailView += '</div>'
+  
+  return detailView
+}
+
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
+module.exports = Util
